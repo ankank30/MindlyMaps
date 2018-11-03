b5251cb2b04f4fd9e21d65d526edcba5ba8633f2
Chinmay-KB <chinmay.kabi@live.com>
2018-11-04 02:02:49 +0530

    Algo for left turn bias implemented

--- app/src/main/java/com/kb/ank/mindlymaps/routing/FetchRouteJson.java
+++ app/src/main/java/com/kb/ank/mindlymaps/routing/FetchRouteJson.java
@@ -1,4 +1,6 @@
 package com.kb.ank.mindlymaps.routing;
 
 public class FetchRouteJson {
+    String test_url="https://maps.googleapis.com/maps/api/directions/json?origin=22.253301,84.879475&destination=22.245609,84.870406&alternatives=true&key=%20AIzaSyAHsNPuWJgiQr0zG51gEaZV1fati3jiRAQ";
+
 }
